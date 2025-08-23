046878ad36895b2d3049845095857cbcdcf05fb7
Roman Sivuda <Romanytsch@gmail.com>
2025-08-23 22:58:05 +0300

    Решены 1 - 5 задачи

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,42 +1,51 @@
-import datetime
+from datetime import datetime, timedelta
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
 
+cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada', 'Mercedes']
+breeds = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
 
 @app.route('/hello_world')
-def test_function():
-    pass
+def hello():
+    return "Привет, мир!"
 
 
 @app.route('/cars')
-def test_function():
-    pass
+def cars():
+    global cars_list
+    return ', '.join(cars_list)
 
 
 @app.route('/cats')
-def test_function():
-    pass
+def cats():
+    global breeds
+    return random.choice(breeds)
 
 
 @app.route('/get_time/now')
-def test_function():
-    pass
+def current_time_func():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return f'«Точное время: {current_time}»'
 
 
 @app.route('/get_time/future')
-def test_function():
-    pass
+def current_time_future():
+    future_time = datetime.now() + timedelta(hours=1)
+    formatted_time = future_time.strftime("%H:%M:%S")
+    return f'«Точное время через час: {formatted_time}»'
 
 
-@app.route('/get_random_word')
-def test_function():
-    pass
-
-
-@app.route('/counter')
-def test_function():
-    pass
+# @app.route('/get_random_word')
+# def test_function():
+#     pass
+#
+#
+# @app.route('/counter')
+# def test_function():
+#     pass
 
 
 if __name__ == '__main__':
