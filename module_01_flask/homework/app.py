526b0ca671874807183973def2bec288f01ddec2
Roman Sivuda <Romanytsch@gmail.com>
2025-08-24 21:44:03 +0300

    Решены 6 и 7 задачи

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,12 +1,30 @@
 from datetime import datetime, timedelta
 import random
-
+import os
+import re
 from flask import Flask
 
 app = Flask(__name__)
 
 cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada', 'Mercedes']
 breeds = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
+
+# Абсолютный путь к папке с проектом и к файлу книги
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+
+COUNTER = 0
+
+# Функция для получения списка слов из текста (без знаков препинания)
+def get_words(txt):
+    words_txt = re.findall(r'\b[а-яА-ЯёЁ]+\b', txt)
+    return words_txt
+
+# Читаем файл и получаем слова один раз при запуске приложения
+with open(BOOK_FILE, encoding='utf-8') as file:
+    text = file.read()
+
+words = get_words(text)
 
 @app.route('/hello_world')
 def hello():
@@ -38,14 +56,17 @@ def current_time_future():
     return f'«Точное время через час: {formatted_time}»'
 
 
-# @app.route('/get_random_word')
-# def test_function():
-#     pass
-#
-#
-# @app.route('/counter')
-# def test_function():
-#     pass
+@app.route('/get_random_word')
+def random_word():
+    word = random.choice(words)
+    return word
+
+
+@app.route('/counter')
+def counter():
+    global COUNTER
+    COUNTER += 1
+    return f'{str(COUNTER)}'
 
 
 if __name__ == '__main__':
