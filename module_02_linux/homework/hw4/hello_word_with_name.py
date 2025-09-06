60931d69f1a0b8ee372e8d4b6b56ec922af9dbac
roma <5139386912@my.lerna.by>
2025-09-06 23:23:06 +0300

    Выполнено 4-е задание

--- module_02_linux/homework/hw4/hello_word_with_name.py
+++ module_02_linux/homework/hw4/hello_word_with_name.py
@@ -1,6 +1,8 @@
 """
-Реализуйте endpoint /hello-world/<имя>, который возвращает строку «Привет, <имя>. Хорошей пятницы!».
-Вместо хорошей пятницы endpoint должен уметь желать хорошего дня недели в целом, на русском языке.
+Реализуйте endpoint /hello-world/<имя>, который возвращает строку «Привет, <имя>.
+Хорошей пятницы!».
+Вместо хорошей пятницы endpoint должен уметь желать хорошего дня недели в целом,
+на русском языке.
 
 Пример запроса, сделанного в субботу:
 
@@ -8,13 +10,27 @@
 """
 
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
+day_names = {
+    0: "Хорошего понедельника",
+    1: "Хорошего вторника",
+    2: "Хорошей среды",
+    3: "Хорошего четверга",
+    4: "Хорошей пятницы",
+    5: "Хорошей субботы",
+    6: "Хорошего воскресенья"
+}
 
-@app.route('/hello-world/...')
-def hello_world():
-    ...
+
+@app.route('/hello-world/<user_name>')
+def hello_world(user_name):
+    weekday = datetime.today().weekday()
+    weekday_names = day_names[weekday]
+    return f"Привет, {user_name}. {weekday_names}!"
+
 
 
 if __name__ == '__main__':
