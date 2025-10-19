e6bde18fd2c16ff6cc1147e63acc6fc73d12a515
roma <5139386912@my.lerna.by>
2025-10-19 14:39:37 +0300

    Выполнено 4-е задание

--- module_05_processes_and_threads/homework/hw4/redirect.py
+++ module_05_processes_and_threads/homework/hw4/redirect.py
@@ -1,27 +1,41 @@
 """
-Иногда возникает необходимость перенаправить вывод в нужное нам место внутри программы по ходу её выполнения.
+Иногда возникает необходимость перенаправить вывод в нужное нам место внутри программы
+по ходу её выполнения.
 Реализуйте контекстный менеджер, который принимает два IO-объекта (например, открытые файлы)
 и перенаправляет туда стандартные потоки stdout и stderr.
 
 Аргументы контекстного менеджера должны быть непозиционными,
 чтобы можно было ещё перенаправить только stdout или только stderr.
 """
-
+import sys
 from types import TracebackType
 from typing import Type, Literal, IO
 
 
 class Redirect:
-    def __init__(self, stdout: IO = None, stderr: IO = None) -> None:
-        ...
+    def __init__(self, *, stdout: IO = None, stderr: IO = None) -> None:
+        self._new_stdout = stdout
+        self._new_stderr = stderr
+        self._old_stdout = None
+        self._old_stderr = None
 
     def __enter__(self):
-        ...
+        if self._new_stdout is not None:
+            self._old_stdout = sys.stdout
+            sys.stdout = self._new_stdout
+        if self._new_stderr is not None:
+            self._old_stderr = sys.stderr
+            sys.stderr = self._new_stderr
+        return self
 
     def __exit__(
-            self,
-            exc_type: Type[BaseException] | None,
-            exc_val: BaseException | None,
-            exc_tb: TracebackType | None
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None
     ) -> Literal[True] | None:
-        ...
+        if self._new_stdout is not None:
+            sys.stdout = self._old_stdout
+        if self._new_stderr is not None:
+            sys.stderr = self._old_stderr
+        return None
