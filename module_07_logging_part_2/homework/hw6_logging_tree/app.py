4325c3abe5ebf30e26e3e35a6cd95fb8927df5c6
roma <5139386912@my.lerna.by>
2025-11-10 23:32:59 +0300

    Выполнено 6-е задание

---
+++ module_07_logging_part_2/homework/hw6_logging_tree/app.py
@@ -0,0 +1,44 @@
+
+import sys
+import logging.config
+from utils import string_to_operator
+# from logger_helper import get_logger
+from logging_config import dict_config
+import logging_tree
+
+
+logging.config.dictConfig(dict_config)
+
+logger = logging.getLogger("calc")
+
+with open("logging_tree.txt", "w", encoding="utf-8") as f:
+    f.write(logging_tree.format.build_description())
+
+def calc(args):
+    logger.info("Arguments: %s", args)
+
+    num_1 = args[0]
+    operator = args[1]
+    num_2 = args[2]
+
+    try:
+        num_1 = float(num_1)
+    except ValueError as e:
+        logger.exception("Error while converting number 1", exc_info=e)
+
+    try:
+        num_2 = float(num_2)
+    except ValueError as e:
+        logger.exception("Error while converting number 2", exc_info=e)
+
+    operator_func = string_to_operator(operator)
+
+    result = operator_func(num_1, num_2)
+
+    logger.info("Result: %s", result)
+    logger.info("%s %s %s = %s", num_1, operator, num_2, result)
+
+
+if __name__ == '__main__':
+    # calc(sys.argv[1:])
+    calc('2+3')
