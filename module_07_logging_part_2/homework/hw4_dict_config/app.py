fb3b35bc2f8a69a4da98a07349a704f4b41e3e2a
roma <5139386912@my.lerna.by>
2025-11-10 23:08:00 +0300

    Выполнено 4-е задание

---
+++ module_07_logging_part_2/homework/hw4_dict_config/app.py
@@ -0,0 +1,40 @@
+
+import sys
+import logging.config
+from utils import string_to_operator
+# from logger_helper import get_logger
+from logging_config import dict_config
+
+
+logging.config.dictConfig(dict_config)
+
+logger = logging.getLogger("calc")
+
+def calc(args):
+    logger.info("Arguments: %s", args)
+
+    num_1 = args[0]
+    operator = args[1]
+    num_2 = args[2]
+
+    try:
+        num_1 = float(num_1)
+    except ValueError as e:
+        logger.exception("Error while converting number 1", exc_info=e)
+
+    try:
+        num_2 = float(num_2)
+    except ValueError as e:
+        logger.exception("Error while converting number 2", exc_info=e)
+
+    operator_func = string_to_operator(operator)
+
+    result = operator_func(num_1, num_2)
+
+    logger.info("Result: %s", result)
+    logger.info("%s %s %s = %s", num_1, operator, num_2, result)
+
+
+if __name__ == '__main__':
+    # calc(sys.argv[1:])
+    calc('2+3')
---
+++ module_07_logging_part_2/homework/hw4_dict_config/logger_helper.py
@@ -0,0 +1,52 @@
+import logging
+import sys
+
+
+class LevelFileHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.files = {
+            logging.DEBUG: open('calc_debug.log', 'a', encoding='utf-8'),
+            logging.INFO: open('calc_info.log', 'a', encoding='utf-8'),
+            logging.WARNING: open('calc_warning.log', 'a', encoding='utf-8'),
+            logging.ERROR: open('calc_error.log', 'a', encoding='utf-8'),
+            logging.CRITICAL: open('calc_critical.log', 'a', encoding='utf-8'),
+        }
+
+    def emit(self, record):
+
+        if record.levelno == logging.CRITICAL:
+            file = self.files[logging.CRITICAL]
+        elif record.levelno == logging.ERROR:
+            file = self.files[logging.ERROR]
+        elif record.levelno == logging.WARNING:
+            file = self.files[logging.WARNING]
+        elif record.levelno == logging.INFO:
+            file = self.files[logging.INFO]
+        elif record.levelno == logging.DEBUG:
+            file = self.files[logging.DEBUG]
+
+        msg = self.format(record)
+        file.write(msg + '\n')
+        file.flush()
+
+    def close(self):
+        for f in self.files.values():
+            f.close()
+        super().close()
+
+# def get_logger(name):
+#     logger = logging.getLogger(name)
+#     logger.setLevel(logging.DEBUG)
+#
+#     formatter = logging.Formatter(
+#         '%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s',
+#         datefmt='%Y-%m-%d %H:%M:%S'
+#     )
+#
+#     level_file_handler = LevelFileHandler()
+#     level_file_handler.setFormatter(formatter)
+#     logger.addHandler(level_file_handler)
+#
+#     return logger
+
