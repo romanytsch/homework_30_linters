5784a31602df5b05b96328a11feaf4914f5a6309
roma <5139386912@my.lerna.by>
2025-12-28 18:58:31 +0300

    Выполнено 3-е задание

--- module_12_multitasking_2/homework/hw3/main.py
+++ module_12_multitasking_2/homework/hw3/main.py
@@ -1,30 +1,42 @@
 from threading import Semaphore, Thread
 import time
+import sys
 
-sem: Semaphore = Semaphore()
-
+sem = Semaphore(1)  # Явно указано значение 1
+stop_threads = False
 
 def fun1():
+    global stop_threads
     while True:
         sem.acquire()
+        if stop_threads:
+            sem.release()
+            break
         print(1)
         sem.release()
         time.sleep(0.25)
 
-
 def fun2():
+    global stop_threads
     while True:
         sem.acquire()
+        if stop_threads:
+            sem.release()
+            break
         print(2)
         sem.release()
         time.sleep(0.25)
 
+t1 = Thread(target=fun1, daemon=True)
+t2 = Thread(target=fun2, daemon=True)
 
-t1: Thread = Thread(target=fun1)
-t2: Thread = Thread(target=fun2)
+t1.start()
+t2.start()
+
 try:
-    t1.start()
-    t2.start()
+    while True:
+        time.sleep(1)
 except KeyboardInterrupt:
-    print('\nReceived keyboard interrupt, quitting threads.')
-    exit(1)
+    print('\nCtrl+C!')
+    stop_threads = True
+    sys.exit(0)
