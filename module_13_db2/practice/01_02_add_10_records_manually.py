9f7fa32de3ad884eb642f01197ba7a33238f848c
Roman Sivuda <Romanytsch@gmail.com>
2026-01-08 22:42:18 +0300

    Выполнено 1-е задание

--- module_13_db2/practice/01_02_add_10_records_manually.py
+++ module_13_db2/practice/01_02_add_10_records_manually.py
@@ -2,11 +2,17 @@ import sqlite3
 
 
 def add_10_records_to_table_warehouse(cursor: sqlite3.Cursor) -> None:
-    ...
+    for item in range(10):
+        cursor.execute("""
+        INSERT INTO `table_warehouse` (name, description, amount)
+        VALUES (?, ?, ?)
+    """, ('Помидоры', 'Кучерявые', f'{item * 100 + 500}')
+    )
+
 
 
 if __name__ == "__main__":
-    with sqlite3.connect("practise.db") as conn:
+    with sqlite3.connect(r"C:\Users\ProBook\PycharmProjects\python_advanced\module_13_db2\materials\db_1.db") as conn:
         cursor = conn.cursor()
         add_10_records_to_table_warehouse(cursor)
         conn.commit()
--- module_13_db2/practice/01_03_add_books_from_file.py
+++ module_13_db2/practice/01_03_add_books_from_file.py
@@ -2,7 +2,33 @@ import sqlite3
 
 
 def add_books_from_file(c: sqlite3.Cursor, file_name: str) -> None:
-    ...
+    with open(file_name, 'r', encoding='utf-8') as file:
+        first_line = True
+        for i_line in file:
+            i_line = i_line.strip()
+
+            if not i_line:  # пропускаем пустые строки
+                continue
+
+            data = i_line.split(',')
+
+            if first_line:
+                first_line = False
+                continue
+
+            if len(data) != 4:
+                print(f"Пропуск некорректной строки: {i_line}")
+                continue
+
+            isbn = data[0].strip()
+            book_name = data[1].strip()
+            author = data[2].strip()
+            publish_year = data[3].strip()
+
+            c.execute("""
+            INSERT INTO `table_books` (isbn, book_name, author, publish_year)
+            VALUES (?, ?, ?, ?)
+            """, (isbn, book_name, author, publish_year))
 
 
 if __name__ == "__main__":
