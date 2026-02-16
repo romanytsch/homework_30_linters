d5d0b41bfa64fd77800eb3324a0e56cd71e1dacb
Roman Sivuda <Romanytsch@gmail.com>
2026-02-16 21:53:06 +0300

    Выполнено 1-е задание

--- module_13_db2/homework/hw1/main.py
+++ module_13_db2/homework/hw1/main.py
@@ -1,29 +1,33 @@
 import sqlite3
 
-get_vaccine = """
-SELECT EXISTS(
-    SELECT 1 
-    FROM table_truck_with_vaccine 
-    WHERE truck_number = ?
-      AND temperature_in_celsius NOT BETWEEN -20 AND -16
-)
-"""
 
 def check_if_vaccine_has_spoiled(
         cursor: sqlite3.Cursor,
         truck_number: str
 ) -> bool:
-    cursor.execute(get_vaccine, (truck_number,))
 
+    query = """
+        SELECT EXISTS(
+            SELECT 1 FROM table_truck_with_vaccine t1
+            WHERE t1.truck_number = ?
+              AND t1.temperature_in_celsius NOT BETWEEN -20 AND -16
+              AND EXISTS(
+                  SELECT 1 FROM table_truck_with_vaccine t2
+                  WHERE t2.truck_number = t1.truck_number
+                    AND t2.temperature_in_celsius NOT BETWEEN -20 AND -16
+                    AND t2.timestamp >= datetime(t1.timestamp, '-2 hours')
+                    AND t2.timestamp <= t1.timestamp
+              )
+        )
+        """
+    cursor.execute(query, (truck_number,))
     return cursor.fetchone()[0] == 1
-
 
 
 if __name__ == '__main__':
     truck_number: str = input('Введите номер грузовика: ')
-    with sqlite3.connect(r'C:\Users\ProBook\PycharmProjects\python_advanced\module_13_db2\homework\homework.db') as conn:
+    with sqlite3.connect('../homework.db') as conn:
         cursor: sqlite3.Cursor = conn.cursor()
         spoiled: bool = check_if_vaccine_has_spoiled(cursor, truck_number)
         print('Испортилась' if spoiled else 'Не испортилась')
-
         conn.commit()
