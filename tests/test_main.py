fc02fae9cbeecb3b44d3b7f860b500180c1f79d7
Roman Sivuda <Romanytsch@gmail.com>
2026-03-09 23:43:58 +0300

    Fix tests and remove bad init files

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,20 @@
+import pytest
 from fastapi.testclient import TestClient
+from unittest.mock import patch
+import main
 
-from main import app
+client = TestClient(main.app)
 
-client = TestClient(app)
+@pytest.fixture
+def mock_db():
+    with patch('main.SessionLocal'):
+        yield
 
-
-def test_create_recipe():
-    response = client.post(
-        "/recipes/",
-        json={
-            "title": "Тест",
-            "cooking_time": 60,
-            "ingredients": "ингредиенты",
-            "description": "описание",
-        },
-    )
+def test_create_recipe(mock_db):
+    response = client.post("/recipes/", json={
+        "title": "Тест",
+        "cooking_time": 60,
+        "ingredients": "ингредиенты",
+        "description": "описание"
+    })
     assert response.status_code == 201
